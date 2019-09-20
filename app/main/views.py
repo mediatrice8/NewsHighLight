deada63bb26fb2370bcdaf5510ce9f90ed55ab37
mediatrice8 <kampiremediatrice@gmail.com>
2019-09-20 12:31:17 +0200

    i create a class of source and article

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..requests import get_source
+from ..requests import get_source,get_articles
 # from .forms import ReviewForm
-from ..models import Source
+from ..models import Source, Articles
 
 # Views
 @main.route('/')
@@ -18,3 +18,18 @@ def index():
     health = get_source('health')
     title = ' Welcome to the online Newshighlights'
     return render_template('index.html', title = title, sports =sports, entertainment=entertainment, technology=technology, business=business, general=general,health = health)
+@main.route('/source/<int:id>')
+def source(id):
+
+    sources = get_source(id)
+    # title = f'{id} | All Article'
+    return render_template('index.html',source = sources)
+
+@main.route('/article/<source_id>')
+def articles(source_id):
+    '''
+    Function that returns the articles
+    '''
+    articles_source = get_articles(source_id)
+    # title = f'{id}| Articles'
+    return render_template('articles.html', articles = articles_source)
--- app/requests.py
+++ app/requests.py
@@ -1,20 +1,21 @@
 import urllib.request,json
-from .models import Source
+from .models import Source,Articles
 
 # Source= source.Source
 # Getting api key
-api_key = None
-# Getting the movie base url
-base_url = None
+# api_key = None
+# # Getting the movie base url
+# base_url = None
+# articles_url = None
 
 
 def configure_request(app):
-    global api_key,base_url
+    global api_key,base_url,articles_url
     api_key = app.config['SOURCE_API_KEY']
     base_url = app.config['SOURCE_API_BASE_URL']
-    print('lslsll')
-    # print(api_key)
-    # print(base_url)
+    articles_url = app.config['ARTICLES_BASE_URL']
+    # news_headlines_url = app.config['NEWS_HEADLINES_BASE_URL']
+    # search_url = app.config['SEARCH_BASE_URL']
     
 def get_source(category):
     '''
@@ -35,10 +36,10 @@ def get_source(category):
     return source_results
 def process_results(source_list):
     '''
-    Function  that processes the movie result and transform them to a list of Objects
+    Function  that processes the source result and transform them to a list of Objects
 
     Args:
-        movie_list: A list of dictionaries that contain source details
+        source_list: A list of dictionaries that contain source details
 
     Returns :
         sources_results: A list of news objects
@@ -57,3 +58,41 @@ def process_results(source_list):
         source_results.append(source_object)
 
     return source_results
+def get_articles(source_id):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_article_url =articles_url.format(source_id,api_key)
+    print(get_article_url)
+
+    with urllib.request.urlopen(get_article_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+        
+ 
+        articles_results = None
+
+        if get_articles_response['articles']:
+            articles_results_list = get_articles_response['articles']
+            articles_results = process_articles(articles_results_list)
+
+    return articles_results
+
+def process_articles(articles_list):
+    '''
+    Function  that processes the article result and transform them to a list of Objects
+    '''
+    articles_result = []
+    for article_item in articles_list:
+        author = article_item.get('author')
+        title = article_item.get('title')
+        description = article_item.get('description')
+        url = article_item.get('url')
+        urlToImage = article_item.get('urlToImage')
+        publishedAt = article_item.get('publishedAt')
+        
+        if urlToImage:
+            article_object = Articles(author,title,description,url,urlToImage,publishedAt)
+            articles_result.append(article_object)
+    return articles_result
+
