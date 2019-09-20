04aadb61502e32db5105e902d27434d0be025438
mediatrice8 <kampiremediatrice@gmail.com>
2019-09-20 12:39:57 +0200

    test for article and source class

--- tests/test_source.py
+++ tests/test_source.py
@@ -1,16 +1,31 @@
-# import unittest
-# from app.models import Source
+import unittest
+from app.models import Source
 
-# class SourceTest(unittest.TestCase):
-#     '''
-#     Test Class to test the behaviour of the source class
-#     '''
+class TestNews(unittest.TestCase):
+    '''
+    Test class to test the behavior of the articles class
+    '''
+    def setUp(self):
+        '''
+        Test class to run before other tests
+        '''
+        self.new_source = Source('abc','liz','https://abc.com/','abc news is the best source', 'usa', 'general', 'abc-news')
+    
+    def test_instance(self):
+        '''
+        '''
+        self.assertTrue(isinstance(self.new_source,Source))
+    
+    def test_to_check_instance_variables(self):
+        '''
+        '''
+        self.assertEquals(self.new_source.name,'abc')
+        self.assertEquals(self.new_source.url,'https://abc.com/')
+        self.assertEquals(self.new_source.description,'abc news is the best source')
+        self.assertEquals(self.new_source.country,'usa')
+        self.assertEquals(self.new_source.category,'general')
+        self.assertEquals(self.new_source.id,'abc-news')
+        
+        
+        
 
-#     def setUp(self):
-#         '''
-#         Set up method that will run before every Test
-#         '''
-#         self.new_sources = Sources(1234,'Python Must Be Crazy','A thrilling new Python Series','/khsjha27hbs',8.5,129993)
-
-    # def test_instance(self):
-    #     self.assertTrue(isinstance(self.new_movie,Movie))
