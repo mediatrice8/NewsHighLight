a3cba0c6e42984baec400b232aa254bd590c0848
mediatrice8 <kampiremediatrice@gmail.com>
2019-09-20 13:05:28 +0200

    i configure all url

--- config.py
+++ config.py
@@ -3,9 +3,11 @@ import os
 class Config:
 
     SOURCE_API_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
-    # SOURCE_API_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
+    ARTICLES_BASE_URL ='https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    # NEWS_HEADLINES_BASE_URL = 'https://newsapi.org/v2/news_headlines?source=abc-news,aftenposten,abc-news-au,al-jazeera-english,ansa,argaam,ars-technica,ary-news,associated-pres,australian-financial-review,independent&pageSize={}&apiKey={}'
+    # SEARCH_BASE_URL = 'https://newsapi.org/v2/search?q={}&pageSize={}&apiKey={}'
     SOURCE_API_KEY  = '68df0bb9d5d44acda7ef91bb0d77b229'
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class ProdConfig(Config):
