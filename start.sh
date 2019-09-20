16c90cabb13e2f380f3b1958794c152e59a9dae2
mediatrice8 <kampiremediatrice@gmail.com>
2019-09-20 13:29:37 +0200

    update

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 export SOURCE_API_KEY = 68df0bb9d5d44acda7ef91bb0d77b229
-export SECRET_KEY = a random string
+# export SECRET_KEY = a random string
 python3.6 manage.py server
